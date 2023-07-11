28d2f444056c2d99d312f0e3a58ba1338512bd14
umut-turk <113532432+umut-turk@users.noreply.github.com>
2023-07-11 21:07:36 +0300

    Girilen-Sayinin-Yuzdesini-Bulma

---
+++ yüzde bülma/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yüzde_bülma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("yüzdesi bulnacak sayı");
+            string girensayı = Console.ReadLine();
+            int sayı=Convert.ToInt32(girensayı);
+
+
+
+
+
+            Console.WriteLine("yüzde  kac");
+            string girenyüzde = Console.ReadLine();
+            int yüzde = Convert.ToInt32(girenyüzde);
+
+            int sonuc = (sayı * yüzde) / 100;
+            Console.WriteLine(sayı + " " +"sayısının" +" " +"yüzde" + " "+  girenyüzde+"ü" + " "+sonuc   );
+
+            Console.ReadLine();
+
+        }
+    }
+}
